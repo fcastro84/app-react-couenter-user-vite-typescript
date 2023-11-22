a842df00f1e637c6af54d09bb8d12c146e7be0e6
Fidel <fenrique.castro@gmail.com>
2023-11-22 10:29:33 -0500

    useEffect and useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Counter from './components/Counter';
 import User from './components/User';
 
+import TimerParent from './components/TimerParent';
+
 
 function App() {
 
@@ -8,8 +10,14 @@ function App() {
     <>
       <h1>Vite + React + TS</h1>
       <hr />
+      <h2> useState </h2>
       <Counter />
       <User />
+
+      <h2 className='mt-4'> useEffect - useRef </h2>
+      <hr />
+
+      <TimerParent />
     </>
   )
 }
